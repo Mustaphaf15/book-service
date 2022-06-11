6227e977f89290d3ba23679a8f29f95c9ec779ff
musss <mustapha.f15@gmail.com>
2022-06-11 04:53:59 +0100

    add DiscoveryClientRouteDefinitionLocator

--- discovery-service/src/main/java/com/example/discoveryservice/DiscoveryServiceApplication.java
+++ discovery-service/src/main/java/com/example/discoveryservice/DiscoveryServiceApplication.java
@@ -2,7 +2,11 @@ package com.example.discoveryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
 
 @EnableEurekaServer
 @SpringBootApplication
@@ -12,4 +16,10 @@ public class DiscoveryServiceApplication {
         SpringApplication.run(DiscoveryServiceApplication.class, args);
     }
 
+    DiscoveryClientRouteDefinitionLocator discoveryClientRouteDefinitionLocator(
+            ReactiveDiscoveryClient rdc,
+            DiscoveryLocatorProperties properties
+    ){
+        return new DiscoveryClientRouteDefinitionLocator(rdc,properties);
+    }
 }
